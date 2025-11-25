9cfd599ebb04156520aeba1fc5b7cfa210187554
mayma288 <oumaima.jlejla@issatm.ucar.tn>
2025-11-25 01:14:47 +0100

    added summary dto

---
+++ Backend/DTOs/TaskSummaryDto.cs
@@ -0,0 +1,9 @@
+namespace smart_task_manager.DTOs
+{
+    public class TaskSummaryDto
+    {
+        public int Total { get; set; }
+        public int Completed { get; set; }
+        public int Overdue { get; set; }
+    }
+}
