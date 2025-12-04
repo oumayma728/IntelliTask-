a05ae898cbaa85f7773a4a444ff951128b74caf1
mayma288 <oumaima.jlejla@issatm.ucar.tn>
2025-12-04 14:41:42 +0100

    added login unit tests

--- SmartTaskManager.Backend.Tests/Unit/AuthServiceTests.cs
+++ SmartTaskManager.Backend.Tests/Unit/AuthServiceTests.cs
@@ -50,7 +50,7 @@ namespace SmartTaskManager.Backend.Tests.Unit
 
             // Mock IConfiguration
             _configurationMock = new Mock<IConfiguration>();
-            _configurationMock.Setup(c => c["Jwt:Secret"]).Returns("supersecretkey123456");
+            _configurationMock.Setup(c => c["Jwt:Secret"]).Returns("ThisIsATestSecretKeyForUnitTests12345");
 
             // Create AuthService instance with mocks
             _authService = new AuthService(
@@ -74,9 +74,50 @@ namespace SmartTaskManager.Backend.Tests.Unit
             // Assert
             Assert.True(result.Succeeded);
         }
+        [Fact]
+        public async Task RegisterAsync_ShouldReturnFailed_whenUserCreatedFails()
+        {
+            _userManagerMock
+                .Setup(u => u.CreateAsync(It.IsAny<User>(),It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Failed(new IdentityError { Description = "User creation failed" }));
+            // Act
+            var result = await _authService.RegisterAsync("test@gmail.com", "123");
 
-       
+            // Assert
+            Assert.False(result.Succeeded);
+        }
 
+        [Fact] 
+        public async Task LoginAsync_ShouldReturnToken_WhenLoginSuccessful()
+        {
+            var fakeUser = new User
+            {
+                Email = "test@gmail.com",
+                UserName = "test",
+                Id = "123",
+                Mode = "personal"
+            };
+
+            //find user by email returns fake user
+            _userManagerMock
+                .Setup(u => u.FindByEmailAsync("test@gmail.com"))
+                .ReturnsAsync(fakeUser);    
+
+            _signInManagerMock
+                .Setup(s => s.CheckPasswordSignInAsync(fakeUser,"correctpass" ,false))
+                .ReturnsAsync(SignInResult.Success);
+
+
+            var response = await _authService.LoginAsync("test@gmail.com", "correctpass");
+            Assert.NotNull(response);
+            Assert.NotNull(response.Token);
+            Assert.Equal("test@gmail.com",response.Email);
+            Assert.Equal("test", response.Username);
+            Assert.Equal("personal", response.Mode);    
+
+
+
+        }
+        }
     
     }
-}
